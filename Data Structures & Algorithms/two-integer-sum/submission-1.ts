238ff359b44d359343236164540d24eb9be20a55
Sergey-Xursevich <47611557+Sergey-Xursevich@users.noreply.github.com>
2026-07-01 10:31:54 +0200

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const acc: number[] = []
+
+        for (let i = 0; i < nums.length - 1; i++) {
+            for (let j = 1; j < nums.length; j++) {
+                const summ = nums[i] + nums[j]
+
+                console.log(i + "---" + j)
+
+
+                if (summ === target) {
+                    return acc.concat([i, j])
+                }
+            }
+        }
+
+        return null;
+    }
+}
