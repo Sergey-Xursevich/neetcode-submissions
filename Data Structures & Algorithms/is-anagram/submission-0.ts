f6aa63a15253b32fef06d209cffedafa1908c2a2
Sergey-Xursevich <47611557+Sergey-Xursevich@users.noreply.github.com>
2026-07-01 10:51:44 +0200

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        let isAnagram = true
+        const first: { [key: string]: number } = {}
+ 
+        for (let k of s) {
+            first[k] = first[k] ? first[k] + 1 : 1
+        }
+
+        for (let m of t) {
+            if (first[m]) {
+                first[m]--
+            }
+        }
+
+        for (const [key, value] of Object.entries(first)) {
+            if (value > 0) {
+                isAnagram = false
+            }
+        } 
+
+        return isAnagram
+    }
+}
