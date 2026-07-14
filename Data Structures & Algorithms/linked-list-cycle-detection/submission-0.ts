214610c25122d9c0dc0f81428f714a4477c3d036
Sergey Khursevich <47611557+skhursevich@users.noreply.github.com>
2026-07-14 13:04:03 +0200

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head: ListNode | null): boolean {
+        let slow, fast = head;
+
+        do {
+            if (fast === null) {
+                return false;
+            } 
+
+            slow = head.next
+            fast = fast.next.next
+        } while (slow !== fast)
+
+        return true;
+    }
+}
